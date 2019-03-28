30ed7d49b0029a76766c1135f2ed7605ce3d37ec
Josh-Norton <38509431+Josh-Norton@users.noreply.github.com>
2019-03-28 14:58:45 +1300

    hero selection + other workspace contents

---
+++ projects/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Nova Master', altName: 'Henry Hot', power: 'Fire'},
+    { id: 2, name: 'Rush', altName: 'Samantha Speedy', power: 'Speed'},
+    { id: 3, name: 'Croco Joe', altName: 'Joe Johnson', power: 'Crocodile Form'},
+    { id: 4, name: 'Ultra Dude', altName: 'Max Manly', power: 'Strength'},
+    { id: 5, name: 'Mr Chops', altName: 'Barry Blade', power: 'Swords'},
+    { id: 6, name: 'Power Lady', altName: 'Penny Power', power: 'Energy Beams'},
+    { id: 7, name: 'Dr Destructor', altName: 'Betty Boom', power: 'Explosions'},
+    { id: 8, name: 'The Zapper', altName: 'Vicky Volt', power: 'Electrity'},
+    { id: 9, name: 'Meat Shield', altName: 'Terry Tough', power: 'Invincibility'},
+    { id: 10, name: 'Captain Warp', altName: 'Fredrick Flash', power: 'Teleportation'},
+]
