7a45ab215b897c6f1e8f468455c5c2d4a95a068a
Josh-Norton <38509431+Josh-Norton@users.noreply.github.com>
2019-04-03 20:41:09 +1300

    dashboard

--- projects/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ projects/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -4,14 +4,13 @@ import { HeroService } from '../hero.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[];
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
   heroSub: Subscription;
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -23,13 +22,9 @@ export class HeroesComponent implements OnInit {
     this.heroSub.unsubscribe();
   }
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroSub = this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 
 }
